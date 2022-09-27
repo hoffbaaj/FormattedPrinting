58d3ed1f38a66a3bd6e098b7539df3991d8f9cbb
hoffbaaj <hoffbaaj@mail.uc.edu>
2022-09-27 13:31:29 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        double salary = 455678.12345;
+        int age = 18;
+        String name = "Andrew Hoffbauer";
+
+        System.out.printf("%20s%5d%12.2f", name, age, salary);
+    }
+}
